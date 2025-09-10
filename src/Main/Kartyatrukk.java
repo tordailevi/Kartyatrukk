6a5039870149ee58ecc98241417982914971afa7
Tordai Levente Barnabás (Szf_N_2024_2026) <tordai.levente.barnabas@diak.szamalk-szalezi.hu>
2025-09-10 15:19:45 +0200

    kirak és összeállít metódus működik

--- src/Main/Kartyatrukk.java
+++ src/Main/Kartyatrukk.java
@@ -2,49 +2,39 @@
 package Main;
 
 public class Kartyatrukk {
+
     static String[] pakli = new String[22];
+    
 
-    
-    
-    public static void kirak(){
-        for (int i = 0; i < 10; i++) {
-            System.out.print();
-        }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-
-        }
-    
-    
-    
     public static void main(String[] args) {
+        osszeallit();
         kirak();
-        for (int db = 0; db < pakli.length; db++) {
-            System.out.println(pakli[db]);
-        }  
     }
-    
-    private static void osszeallit(){
+
+    public static void osszeallit() {
+        int db = 1;
         String[] szinek = {"P", "T", "Z", "M"};
         String[] ertekek = {"Ász", "Kir", "Fel", "X", "IX", "VIII"};
-        
-        int index = 1;
-        for (int i = 0; i < szinek.length-1; i++) {
-            for (int j = 0; j < ertekek.length; j++) {
-                pakli[index++] = szinek[i] + " " + ertekek[j];
-                index++;
-                }
+
+        for (int i = 0; i < szinek.length-1; i++) { 
+            for (int j = 0; j < ertekek.length; j++) { 
+                pakli[db++] = szinek[i] + " " + ertekek[j];
             }
-        for (int i = 0; i < 3; i++) {
-            pakli[index++] = szinek[3] + " " + ertekek[i];
         }
+        
+        for (int i = 0; i < 3; i++) {
+            pakli[db++] = szinek[3] + "_" + ertekek[i];
         }
     }
+    
+    
+    
+    public static void kirak() {
+        for (int i = 0; i < pakli.length; i++) {
+            System.out.printf("%-7s".formatted(pakli[i]));
+            if (i % 3 == 0) {
+                System.out.println("\n");
+            }
+        }
+    }
+}
